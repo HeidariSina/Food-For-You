59af32a14de9ef0ab494c0d0a5bac75ba9984dfa
Sine Heidari <sinaheidari2000@outlook.com>
2023-06-08 00:07:57 +0430

    add more ingrient and more Meals

--- lib/db/ing.dart
+++ lib/db/ing.dart
@@ -14,18 +14,26 @@ Ingredients brownSugar =
     Ingredients("Brown Sugar", "assets/svg/brown_sugar.svg");
 Ingredients butter = Ingredients("Butter", "assets/svg/butter.svg");
 Ingredients cabbage = Ingredients("Cabbage", "assets/svg/cabbage.svg");
+Ingredients carrot = Ingredients("Carrot", "assets/svg/carrot.svg");
 Ingredients cheese = Ingredients("Cheese", "assets/svg/cheese.svg");
 Ingredients chicken = Ingredients("Chicken", "assets/svg/chicken.svg");
 Ingredients chocolate = Ingredients("Chocolate", "assets/svg/chocolate.svg");
+Ingredients cinnamon = Ingredients("Cinnamon", "assets/svg/cinnamon.svg");
 Ingredients cookie = Ingredients("Cookie", "assets/svg/cookie.svg");
 Ingredients egg = Ingredients("Egg", "assets/svg/egg.svg");
 Ingredients fish = Ingredients("Fish", "assets/svg/fish.svg");
 Ingredients flour = Ingredients("Flour", "assets/svg/flour.svg");
+Ingredients grape = Ingredients("Grape", "assets/svg/grape.svg");
+Ingredients iceCream = Ingredients("Ice Cream", "assets/svg/ice_cream.svg");
+Ingredients lemon = Ingredients("Lemon", "assets/svg/lemon.svg");
 Ingredients meat = Ingredients("Meat", "assets/svg/meat.svg");
 Ingredients milk = Ingredients("Milk", "assets/svg/milk.svg");
+Ingredients nutmeg = Ingredients("Nutmeg", "assets/svg/nutmeg.svg");
 Ingredients nuts = Ingredients("Nuts", "assets/svg/nuts.svg");
 Ingredients oil = Ingredients("Oil", "assets/svg/oil.svg");
+Ingredients orange = Ingredients("Orange", "assets/svg/orange.svg");
 Ingredients pepper = Ingredients("Pepper", "assets/svg/pepper.svg");
+Ingredients raisin = Ingredients("Raisin", "assets/svg/raisin.svg");
 Ingredients redPepper = Ingredients("Red Pepper", "assets/svg/red_pepper.svg");
 Ingredients rise = Ingredients("Rise", "assets/svg/rise.svg");
 Ingredients salt = Ingredients("Salt", "assets/svg/salt.svg");
@@ -33,4 +41,17 @@ Ingredients sausage = Ingredients("Sausage", "assets/svg/sausage.svg");
 Ingredients sugar = Ingredients("Sugar", "assets/svg/sugar.svg");
 Ingredients vanilla = Ingredients("Vanilla", "assets/svg/vanilla.svg");
 Ingredients water = Ingredients("Water", "assets/svg/water.svg");
+Ingredients yogurt = Ingredients("Yogurt", "assets/svg/yogurt.svg");
 Ingredients zaferan = Ingredients("Zaferan", "assets/svg/zaferan.svg");
+
+// information Ingredients
+Ingredients info = Ingredients("More Option", "assets/svg/info.svg");
+Ingredients infoD1 = Ingredients("More Option", "assets/svg/info.svg");
+
+// duplicate Ingredients
+Ingredients lemonD1 = Ingredients("Lemon", "assets/svg/lemon.svg");
+Ingredients butterD1 = Ingredients("Butter", "assets/svg/butter.svg");
+Ingredients butterD2 = Ingredients("Butter", "assets/svg/butter.svg");
+Ingredients sugarD1 = Ingredients("Sugar", "assets/svg/sugar.svg");
+Ingredients sugarD2 = Ingredients("Sugar", "assets/svg/sugar.svg");
+Ingredients vanillaD1 = Ingredients("Vanilla", "assets/svg/vanilla.svg");
